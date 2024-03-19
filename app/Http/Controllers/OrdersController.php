2c30c734cd2537b84738a9a80850e9ed643d7e3c
Vladislav <vtinishov@gmail.com>
2024-03-19 19:00:48 +0500

    init

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+public function indexJson(Request $request)
+{
+    dd($request);
+}
+}
